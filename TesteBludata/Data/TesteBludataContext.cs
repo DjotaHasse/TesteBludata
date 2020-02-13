68562773837f9efb90817ac7841c41afd1d8ef3d
Dejorgenes Hasse <dejorgenes.hasse@bludata.local>
2020-02-13 08:47:03 -0300

    commit de alteracao

---
+++ TesteBludata/Data/TesteBludataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TesteBludata.Models;
+
+namespace TesteBludata.Data
+{
+    public class TesteBludataContext : DbContext
+    {
+        public TesteBludataContext (DbContextOptions<TesteBludataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TesteBludata.Models.Empresa> Empresa { get; set; }
+    }
+}
--- TesteBludata/Startup.cs
+++ TesteBludata/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using TesteBludata.Data;
 
 namespace TesteBludata
 {
@@ -33,6 +35,9 @@ namespace TesteBludata
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TesteBludataContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TesteBludataContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
