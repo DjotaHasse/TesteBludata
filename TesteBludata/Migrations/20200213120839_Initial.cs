2f6b769e111ded0e082edec213ffc528f0ef3ac9
Dejorgenes Hasse <dejorgenes.hasse@bludata.local>
2020-02-13 09:21:06 -0300

    Migration

---
+++ TesteBludata/Migrations/20200213120839_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TesteBludata.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RazaoSocial = table.Column<string>(nullable: true),
+                    CNPJ = table.Column<string>(nullable: true),
+                    UF = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresa", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empresa");
+        }
+    }
+}
